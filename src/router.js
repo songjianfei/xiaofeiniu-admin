ac696669e478c7f9a84260ebd9ffc074615af52a
sjf <395195766@qq.com>
2019-01-30 12:32:36 +0800

    ok

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './views/Login.vue'
 import Main from './views/Main.vue'
+
 import GlobalSettings from './views/GlobalSettings.vue'
 
 import TableList from './views/TableList.vue'
