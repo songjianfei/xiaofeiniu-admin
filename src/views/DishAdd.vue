b428be1c63da2c681c36894c924aab5916488001
SongJianfei <395195766@qq.com>
2019-02-24 12:53:30 +0800

    complete

--- src/views/DishAdd.vue
+++ src/views/DishAdd.vue
@@ -1,5 +1,61 @@
 <template>
   <div class="dishadd">
-    <h1>This is an DishAdd page</h1>
+    <h3>添加菜品</h3>
+    <el-form label-width="100px">
+      <el-form-item label="菜品图片">
+        <el-upload class="my-uploader" :action="uploadAction" :on-success="doUploadSucc" name="dishImg" :show-file-list="false">
+          <img v-if="imgUrl" :src="imgUrl" alt="">
+          <i v-else class="el-icon-plus upload-icon"></i>
+        </el-upload>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      imgUrl:'',//要显示的预览图片地址
+      uploadAction:this.$store.state.globalSettings.apiUrl+'/admin/dish/image',   //可处理文件上传的数据API
+      formData:{
+        title:'',
+        imgUrl:'',
+        price:'',
+        detail:'',
+        categoryId:''
+      }
+    }
+  },
+  methods: {
+    doUploadSucc(res,file){
+      //文件上传成功后，客户端得到响应消息之后的处理函数
+      //res代表服务器端返回的响应消息
+      //file:从INPUT[type=file]中读取的第一个上传文件对象
+      this.formData.imgUrl=res.fileName;
+      this.imgUrl=URL.createObjectURL(file.raw);
+    }
+  },
+}
+</script>
+<style lang="scss">
+.my-uploader .el-upload{
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  width: 250px;
+  height: 177px;
+  overflow: hidden;
+  &:hover{
+    border-color:#409EFF;
+  }
+  .upload-icon{
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+}
+</style>
+
--- src/views/DishList.vue
+++ src/views/DishList.vue
@@ -1,5 +1,44 @@
 <template>
   <div class="dishlist">
-    <h1>This is an DishList page</h1>
+    <el-breadcrumb>
+      <el-breadcrumb-item to="/main">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>菜品管理</el-breadcrumb-item>  
+      <el-breadcrumb-item>菜品列表</el-breadcrumb-item>  
+    </el-breadcrumb>
+    <el-tabs type="border-card">
+      <el-tab-pane v-for="(c,i) in dishList" :key='i'>
+          <span slot="label">
+            <el-badge :value="c.dishList.length">{{c.cname}}</el-badge>
+          </span>
+          <el-row>
+            <el-col v-for="(d,j) in c.dishList" :key="j" :xs="12" :md="6" :lg="4" :xl="3">
+                {{d.title}}
+                <img :src="require('../assets/img/dish/'+d.imgUrl)" style="max-width:100%;">
+            </el-col>
+          </el-row>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      dishList:[]      
+    }
+  },
+  mounted() {
+    //异步获取菜品列表
+    var url=this.$store.state.globalSettings.apiUrl+'/admin/dish';
+    this.$axios.get(url).then(({data})=>{
+      console.log(data);
+      this.dishList=data
+    }).catch(err=>{console.log(err)})
+  },
+}
+</script>
+<style lang="scss">
+.dishlist  .el-breadcrumb{
+  margin-bottom: 20px;
+}
+</style>
